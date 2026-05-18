632ac17dffb0c1873999941add24fdc66ebd2c49
isaac gatheru <igatheru7@gmail.com>
2026-05-18 20:19:18 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav
+      style={{
+        background: "#111827",
+        color: "white",
+        padding: "1rem",
+      }}
+    >
+      <h2>Ecommerce Admin</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
