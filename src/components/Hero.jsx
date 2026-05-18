d230097bb7bb7a6ea2710bfc73a6dcf6faf90310
isaac gatheru <igatheru7@gmail.com>
2026-05-18 23:23:51 +0300

    font size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 
 const Hero = () => {
   return (
-    <div className="bg-[#7d6147]  min-h-screen flex flex-col items-center justify-center text-center py-40 px-6">
-      <h1 className="text-8xl font-bold text-white">Coffee R Us</h1>
+    <div className="bg-[#7d6147]  min-h-screen flex flex-col items-center justify-center text-center py-40 px-6 ">
+      <h1 className="text-4xl font-bold text-gray-900 mb-4">Coffee R Us</h1>
 
-      <p className="text-white mt-4 text-xl ">
+      <p className="text-[#ffffff] mt-4 text-xl ">
         The go to store for your coffee needs
       </p>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 const Sidebar = () => {
   return (
-    <div className="w-full bg-[#7d6147] text-white px-10 py-4 flex justify-between items-center">
+    <div className="w-full bg-[#7d614799] text-white px-10 py-4 flex justify-between items-center h-auto">
       <Link to="/" className="text-[#ffffff] hover:underline">Home</Link>
-      <Link to="/products" className="hover:underline">Shop</Link>
-      <Link to="/admin" className="hover:underline">Admin Portal</Link>
+      <Link to="/products" className="text-[#ffffff] hover:underline">Shop</Link>
+      <Link to="/admin" className="text-[#ffffff] hover:underline">Admin Portal</Link>
     </div>
   )
 }
